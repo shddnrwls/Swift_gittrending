c5b9bf0e4bb563bb6248784697ebe00da9a5efec
shddnrwls <shddnrwls@gmail.com>
2018-05-28 16:29:47 +0900

    Create ParsingProtocool

---
+++ GitTrending/Model/ParsingProtocool.swift
@@ -0,0 +1,51 @@
+//
+//  ParsingProtocool.swift
+//  GitTrending
+//
+//  Created by mac on 2018. 5. 28..
+//  Copyright © 2018년 swift. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import Kanna
+
+enum ParsingProtocool{
+    case all()
+    case swift()
+    
+}
+extension ParsingProtocool{
+    private static let parsingUrl:String = "https://github.com"
+    
+    func getParsing() -> Any{
+        guard let main = URL(string: self.url) else{
+             print("Error: \(self.url) doesn't seem to be a valid URL")
+            return false
+        }
+        do{
+            let lolMain = try String(contentsOf: main, encoding: .utf8)
+            let doc = try HTML(html: lolMain, encoding: .utf8)
+            return doc
+        }
+        catch let error {
+            print(error)
+            return false
+        }
+    }
+    private var baseUrl:String{
+        switch self {
+        case .all(),.swift():
+            return ParsingProtocool.parsingUrl
+        
+        }
+    }
+    private var url:String {
+        switch self {
+        case .all():
+            return self.baseUrl+"/trending"
+        case .swift():
+            return self.baseUrl+"/trending/swift"
+        }
+    }
+}
